a9badd4ca9c863c8cae701e2190e05861f1e321e
Alex Waese-Perlman <squishybanana04@gmail.com>
2021-04-03 03:54:01 -0400

    use .env instead of secrets

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,22 @@
 import TimerBot from './bot';
-import TimerDB from './db.bot'
-import * as fs from 'fs';
 import { getLogger } from './util';
 import * as chalk from 'chalk';
 const logger = getLogger('App', chalk.white);
 
 let apiKey = "";
 
-try {
-	if (process.env.API_KEY) apiKey = process.env.API_KEY;
-	apiKey = fs.readFileSync('/run/secrets/api-key').toString().replace('\n', '');
-}
-catch (err) {
-	if (apiKey == "") {
-		logger.error("Unable to find discord key");
-		process.exit(1);
-	}
+if (process.env.API_KEY) apiKey = process.env.API_KEY;
+if (apiKey == "") {
+	logger.error("Unable to find discord key");
+	process.exit(1);
 }
 
 let mongoConnection = "";
 
-try {
-	if (process.env.MONGO_CONNECTION) mongoConnection = process.env.MONGO_CONNECTION;
-	mongoConnection = fs.readFileSync('/run/secrets/mongo-connection').toString().replace('\n', '');
-}
-catch (err) {
-	if (mongoConnection == "") {
-		logger.error("Unable to find mongo connection");
-		process.exit(1);
-	}
+if (process.env.MONGO_CONNECTION) mongoConnection = process.env.MONGO_CONNECTION;
+if (mongoConnection == "") {
+	logger.error("Unable to find mongo connection");
+	process.exit(1);
 }
 
 logger.info('Credentials found');
